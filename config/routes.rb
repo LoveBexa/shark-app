87a466ee3066798d223b7805725c040edf1f84bd
Lovebexa <rebekahliu@outlook.com>
2019-10-08 14:48:14 +0100

    Shark App Tutorial - First Commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :sharks
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+Rails.application.routes.draw do
+  resources :sharks
+
+  root 'sharks#index' 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
